469a01c3f07bf55050adc8ad63dd6356dea54e4d
Edgars <Edgars.Kaufmanis@nology.io>
2022-12-12 23:11:37 +0000

    fetched multiple api and rendered all 325 beers onto page

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,28 @@ const App = (props) => {
   const [beers, setBeers] = useState([]);
 
   const getBeers = async () => {
-    const response = await fetch("https://api.punkapi.com/v2/beers");
-    const data = await response.json();
-    setBeers(data);
+    const url1 = "https://api.punkapi.com/v2/beers?page=1&per_page=80";
+    const url2 = "https://api.punkapi.com/v2/beers?page=2&per_page=80";
+    const url3 = "https://api.punkapi.com/v2/beers?page=3&per_page=80";
+    const url4 = "https://api.punkapi.com/v2/beers?page=4&per_page=80";
+    const url5 = "https://api.punkapi.com/v2/beers?page=5&per_page=80";
+
+    const responses = await Promise.all([
+      fetch(url1),
+      fetch(url2),
+      fetch(url3),
+      fetch(url4),
+      fetch(url5),
+    ]);
+    const data1 = await responses[0].json();
+    const data2 = await responses[1].json();
+    const data3 = await responses[2].json();
+    const data4 = await responses[3].json();
+    const data5 = await responses[4].json();
+
+    const allData = { data1, data2, data3, data4, data5 };
+
+    setBeers([...data1, ...data2, ...data3, ...data4, ...data5]);
   };
 
   useEffect(() => {
